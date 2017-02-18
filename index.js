841cdc2144620bdb1a72a8b3d13387b04b900441
mubashiralisiddiqui <mubashir22ali@gmail.com>
2017-02-18 16:30:28 +0500

    sending data to monogoose

--- index.js
+++ index.js
@@ -5,15 +5,43 @@ var mongoose = require("mongoose")
 var app = express();
 var a = (process.env.PORT || 3000)
 
-app.get("/", function(req,res,next){
-    res.send("hello world")
+
+///shema and modal  of database////
+
+var studentSchema = new mongoose.Schema({
+    name: String
+});
+var studentModel = mongoose.model("student",studentSchema);
+//Schema and model of database//
+
+
+app.get("/add", function(req,res,next){
+
+    var newstudent = new studentModel({
+        name: "ali"
+    })
+   newstudent.save(function(err,data){
+       if(!err){
+           res.send("saved")
+       }
+       else{
+           res.send("error")
+       }
+   })
+
 })
-app.get("/express", function(req,res,next){
-    res.send("hello express")
-})
-app.get("/port", function(req,res,next){
-    res.send("hello port"+a)
-})
+
+
+
+
+
+
+// app.get("/express", function(req,res,next){
+//     res.send("hello express")
+// })
+// app.get("/port", function(req,res,next){
+//     res.send("hello port"+a)
+// })
 
 
 app.listen(a,function(){
